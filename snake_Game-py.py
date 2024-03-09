12bf0cc998e7cd797416797fbe81e37ce7dcb123
Aswin_SuresH <117917680+ASWINSURESH666@users.noreply.github.com>
2024-03-09 17:51:36 +0530

    Update snake_Game-py.py

--- snake_Game-py.py
+++ snake_Game-py.py
@@ -69,7 +69,7 @@ while running:
         snake_direction = (pixel_width, 0)
 
     for snake_part in snake:
-        pygame.draw.rect(screen, "green", snake_part)
+        pygame.draw.rect(screen, "yellow", snake_part)
 
     pygame.draw.rect(screen, "red", target)
 
